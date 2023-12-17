f7187b726579b539dd40753253c734631daf288c
Vinod Krishna Varma V <98218948+vinodkrishnavarma@users.noreply.github.com>
2023-12-17 14:33:25 +0530

    Update seedjob.groovy

--- job-description/seedjob.groovy
+++ job-description/seedjob.groovy
@@ -1,30 +1,8 @@
-pipeline {
-    agent any
-
-    stages {
-        stage('Fetch Job DSL scripts') {
-            steps {
-                git(
-                    url: 'https://github.com/vinodkrishnavarma/jenkinssamplerepo.git',
-                    branch: 'main',
-                    credentialsId: 'github-credentials' // replace with your credential ID
-                )
-            }
-        }
-        stage('Process Job DSL scripts') {
-            steps {
-                // Use the Process Job DSLs plugin to execute the fetched scripts
-                sh 'jobdsl-plugin process-dsl < jobdsl/job1.groovy'
-            }
-        }
-    }
-
-    post {
-        success {
-            echo 'Job DSL scripts processed successfully!'
-        }
-        failure {
-            echo 'Job DSL processing failed!'
+pipelineJob('python_dsl_job_example') {
+    definition {
+        cps {
+            script(readFileFromWorkspace('desire_pipeline.groovy'))
+            sandbox()
         }
     }
 }
