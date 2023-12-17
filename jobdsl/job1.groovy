9a354ade2354215f1b0693e1942fc7bc2db798c8
Vinod Krishna Varma V <98218948+vinodkrishnavarma@users.noreply.github.com>
2023-12-17 14:28:13 +0530

    Update job1.groovy

--- jobdsl/job1.groovy
+++ jobdsl/job1.groovy
@@ -1,25 +1,9 @@
 pipeline {
-    agent any
-
+    agent { label 'slave' }
     stages {
-        stage('List folders in /tmp') {
+        stage('build') {
             steps {
-                script {
-                    // Use Groovy File class to access the directory.
-                    def tmpDir = new File('/tmp')
-
-                    // Get a list of subdirectories.
-                    def subdirs = tmpDir.listFiles(File::isDirectory)
-
-                    // If the directory is empty, inform the user.
-                    if (subdirs.size() == 0) {
-                        echo '/tmp is empty.'
-                    } else {
-                        // If folders exist, print them one per line.
-                        echo "Folders in /tmp:"
-                        subdirs.each { dir -> echo "- $dir.name" }
-                    }
-                }
+                echo "Hello World!"
             }
         }
     }
