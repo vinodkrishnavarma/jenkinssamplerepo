6154762e41c35d17f12a1f40253ce1ef5fe5f3c8
Vinod Krishna Varma V <98218948+vinodkrishnavarma@users.noreply.github.com>
2023-12-17 13:44:31 +0530

    Update seedjob.groovy

--- job-description/seedjob.groovy
+++ job-description/seedjob.groovy
@@ -1,31 +1,30 @@
-
 pipeline {
     agent any
 
     stages {
-        stage('cloning repo ... ') {
+        stage('Fetch Job DSL scripts') {
             steps {
-                git 'https://github.com/alicommit-malp/jenkins_dsl_example'
+                git(
+                    url: 'https://github.com/vinodkrishnavarma/jenkinssamplerepo.git',
+                    branch: 'main',
+                    credentialsId: 'github-credentials' // replace with your credential ID
+                )
             }
         }
-
-        stage('build'){
-            agent{
-                docker { 
-                    image 'python:2' 
-                    reuseNode true
-                }    
-            }
-            steps{
-                sh "pip install -r $WORKSPACE/python/requirements.txt"
-                sh "python $WORKSPACE/python/main.py"
+        stage('Process Job DSL scripts') {
+            steps {
+                // Use the Process Job DSLs plugin to execute the fetched scripts
+                sh 'jobdsl-plugin process-dsl < jobdsl/job1.groovy'
             }
         }
-
     }
-    post{
-        success{
-            echo "Cool :)"
+
+    post {
+        success {
+            echo 'Job DSL scripts processed successfully!'
+        }
+        failure {
+            echo 'Job DSL processing failed!'
         }
     }
 }
