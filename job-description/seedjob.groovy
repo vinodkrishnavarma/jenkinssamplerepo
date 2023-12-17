130bec7e4df1f0bcc3c4b723de508093bcc2d82b
Vinod Krishna Varma V <98218948+vinodkrishnavarma@users.noreply.github.com>
2023-12-17 13:34:06 +0530

    Update seedjob.groovy

--- job-description/seedjob.groovy
+++ job-description/seedjob.groovy
@@ -1,1 +1,31 @@
 
+pipeline {
+    agent any
+
+    stages {
+        stage('cloning repo ... ') {
+            steps {
+                git 'https://github.com/alicommit-malp/jenkins_dsl_example'
+            }
+        }
+
+        stage('build'){
+            agent{
+                docker { 
+                    image 'python:2' 
+                    reuseNode true
+                }    
+            }
+            steps{
+                sh "pip install -r $WORKSPACE/python/requirements.txt"
+                sh "python $WORKSPACE/python/main.py"
+            }
+        }
+
+    }
+    post{
+        success{
+            echo "Cool :)"
+        }
+    }
+}
