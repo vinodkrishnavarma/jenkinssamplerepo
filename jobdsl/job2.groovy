f4fad8a424a32c3e5e7c4cbeed8fb6be48d22aec
Vinod Krishna Varma V <98218948+vinodkrishnavarma@users.noreply.github.com>
2023-12-18 09:32:23 +0530

    Update job2.groovy

--- jobdsl/job2.groovy
+++ jobdsl/job2.groovy
@@ -4,12 +4,14 @@ pipeline {
 stage('test job') {
 
             steps {
-                if (params.dryRun) {
-                echo 'Dry run selected, skipping actual execution.'
-                } else {
-                // Your normal pipeline steps go here...
-                 echo 'Dry run Notselected, .. actual execution.'
-            }
+                step {
+                    if (params.dryRun) {
+                    echo 'Dry run selected, skipping actual execution.'
+                    } else {
+                    // Your normal pipeline steps go here...
+                     echo 'Dry run Notselected, .. actual execution.'
+                    }
+                }
             }
         }
     }
