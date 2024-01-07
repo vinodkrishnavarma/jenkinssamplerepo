c887c982d758bdd6e8d74bf07ffd7221b2f8c201
Vinod Krishna Varma V <98218948+vinodkrishnavarma@users.noreply.github.com>
2024-01-07 11:22:17 +0530

    Update testjob1.groovy

--- job-description/testjob1.groovy
+++ job-description/testjob1.groovy
@@ -6,14 +6,36 @@ pipelineJob('scale-pods') {
     }
   }
     parameters {
-        activeChoiceParam('CHOICE-1') {
-            description('Allows user choose from multiple choices')
+        activeChoiceParam('States') {
+            description('Select a state option')
             filterable()
             choiceType('SINGLE_SELECT')
             groovyScript {
-                script('["choice1", "choice2"]')
-                fallbackScript('"fallback choice"')
+                script('["Sao Paulo", "Rio de Janeiro", "Parana:selected", "Acre"]')
+                fallbackScript('return ["ERROR"]')
             }
+        }
+        activeChoiceReactiveParam('Cities') {
+            description('Active Choices Reactive parameter')
+            filterable()
+            choiceType('CHECKBOX')
+            groovyScript {
+                script('''
+                    if (States.equals('Sao Paulo')) {
+                      return ['Barretos', 'Sao Paulo', 'Itu'];
+                    } else if (States.equals('Rio de Janeiro')) {
+                      return ['Rio de Janeiro', 'Mangaratiba']
+                    } else if (States.equals('Parana')) {
+                      return ['Curitiba', 'Ponta Grossa']
+                    } else if (States.equals('Acre')) {
+                      return ['Rio Branco', 'Acrelandia']
+                    } else {
+                      return ['Unknown state']
+                    }
+                       ''')
+                fallbackScript('return ["Script error!"]')
+            }
+            referencedParameter('States')
         }
     }
 }
