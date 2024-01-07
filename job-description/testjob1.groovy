1d5a26c418c3b193fd88fff0a16665dc0840b657
Vinod Krishna Varma V <98218948+vinodkrishnavarma@users.noreply.github.com>
2024-01-07 09:26:23 +0530

    Update testjob1.groovy

--- job-description/testjob1.groovy
+++ job-description/testjob1.groovy
@@ -5,10 +5,10 @@ pipelineJob('scale-pods') {
         sandbox()
     }
   }
-    // parameters {
-    //     choice(choices: ['Scale Up', 'Scale Down'], description: 'Select scaling action', name: 'scalingAction')
-    //     choice(choices: ['cluster-one', 'cluster-two'], description: 'Select target Kubernetes cluster', name: 'clusterName')
-    //     stringParam(description: 'Name of the deployment to scale', name: 'deploymentName')
-    //     stringParam(defaultValue: '1', description: 'Desired number of replicas', name: 'replicaCount')
-    // }
+    parameters {
+        // choice(choices: ['Scale Up', 'Scale Down'], description: 'Select scaling action', name: 'scalingAction')
+        // choice(choices: ['cluster-one', 'cluster-two'], description: 'Select target Kubernetes cluster', name: 'clusterName')
+        stringParam(description: 'Name of the deployment to scale', name: 'deploymentName')
+        stringParam(defaultValue: '1', description: 'Desired number of replicas', name: 'replicaCount')
+    }
 }
