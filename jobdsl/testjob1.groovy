e6d7a47e4d1e7ce1f8bf4069edc357f04997ed2f
Vinod Krishna Varma V <98218948+vinodkrishnavarma@users.noreply.github.com>
2024-01-07 08:10:59 +0530

    Create testjob1.groovy

---
+++ jobdsl/testjob1.groovy
@@ -0,0 +1,18 @@
+pipeline {
+    agent { label 'built-in' }
+    stages {
+        stage('test job') {
+
+            steps {
+                step ('Run') {
+                    if (params.dryRun) {
+                    echo 'Dry run selected, skipping actual execution.'
+                    } else {
+                    // Your normal pipeline steps go here...
+                     echo 'Dry run Notselected, .. actual execution.'
+                    }
+                }
+            }
+        }
+    }
+}
